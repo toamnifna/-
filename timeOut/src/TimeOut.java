a8ab1179b74c4aa494e71f277d3d1a4be33bf91c
Tao Minfan <94295333+toamnifna@users.noreply.github.com>
2022-11-10 14:26:55 +0800

    Add files via upload

---
+++ timeOut/src/TimeOut.java
@@ -0,0 +1,16 @@
+import menu.menu.BeginMenu;
+import java.io.IOException;
+
+/**
+ * @author 陶敏帆
+ * @version 1.0
+ */
+public class TimeOut {
+
+    public static void main(String[] args) throws IOException {
+
+        BeginMenu.getInstance().show();
+
+    }
+
+}
